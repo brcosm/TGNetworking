20f11063d667ea2477504071957f439641ab96a7
Brandon Smith <brandon.cole.smith@gmail.com>
2012-06-28 20:14:53 -0700

    Initial Commit

---
+++ TGNetworking/TGNetworking.h
@@ -0,0 +1,13 @@
+//
+//  TGNetworking.h
+//  TGNetworking
+//
+//  Created by Brandon Smith on 6/28/12.
+//  Copyright (c) 2012 TokenGnome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TGNetworking : NSObject
+
+@end
---
+++ TGNetworking/TGNetworking.m
@@ -0,0 +1,13 @@
+//
+//  TGNetworking.m
+//  TGNetworking
+//
+//  Created by Brandon Smith on 6/28/12.
+//  Copyright (c) 2012 TokenGnome. All rights reserved.
+//
+
+#import "TGNetworking.h"
+
+@implementation TGNetworking
+
+@end
